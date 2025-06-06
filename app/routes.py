01aa3377646a644af16305bb3b795576a764e0e7
Alelimaprog <importusabraz@gmail.com>
2025-06-06 01:16:32 -0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,26 @@
-# routes.py
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 router = APIRouter()
-@router.get('/admin')
-def admin(): return {'msg': 'admin ok'}
+templates = Jinja2Templates(directory="templates")
+
+@router.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@router.get("/cadastro", response_class=HTMLResponse)
+async def cadastro(request: Request):
+    return templates.TemplateResponse("cadastro.html", {"request": request})
+
+@router.get("/confirmado", response_class=HTMLResponse)
+async def confirmado(request: Request):
+    return templates.TemplateResponse("confirmado.html", {"request": request})
+
+@router.get("/painel", response_class=HTMLResponse)
+async def painel(request: Request):
+    return templates.TemplateResponse("admin.html", {"request": request})
+
+@router.get("/enviar", response_class=HTMLResponse)
+async def enviar():
+    return HTMLResponse("<h2>Envio funcionando corretamente</h2>")
