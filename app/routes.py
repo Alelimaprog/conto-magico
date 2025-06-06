25ff20be6df6c34738047a5342707f18fc7caece
Alelimaprog <importusabraz@gmail.com>
2025-06-06 02:51:36 -0300

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from app.utils import enviar_conto_diario
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -9,14 +10,15 @@ templates = Jinja2Templates(directory="templates")
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@router.get("/painel", response_class=HTMLResponse)
-def painel(request: Request):
-    return templates.TemplateResponse("admin.html", {"request": request})
-
 @router.get("/cadastro", response_class=HTMLResponse)
 def cadastro(request: Request):
     return templates.TemplateResponse("cadastro.html", {"request": request})
 
-@router.get("/admin", response_class=HTMLResponse)
-def admin(request: Request):
-    return templates.TemplateResponse("admin.html", {"request": request})
+@router.post("/cadastro")
+async def processar_cadastro(request: Request, telefone: str = Form(...)):
+    await enviar_conto_diario(telefone)
+    return templates.TemplateResponse("confirmado.html", {"request": request})
+
+@router.get("/enviar", response_class=HTMLResponse)
+def enviar(request: Request):
+    return templates.TemplateResponse("confirmado.html", {"request": request})
